451903025262a4247c45da3e4264ba573c7f1f5d
ziziw <ziwangchen@hotmail.com>
2019-05-11 15:08:46 -0400

    day11

---
+++ day11/day11.js
@@ -0,0 +1,69 @@
+function powerOn(){
+  ship.powerOn = true;
+}
+
+function countModules(){ 
+  return availableModules.length;
+}
+
+function countEssential(){
+  let counter = 0;
+  for(let x = 0; x < countModules(); x++){
+    if(availableModules[x].essential){
+      counter++;
+    }
+  }
+  return counter;
+}
+
+function indexLifeSupport(){
+  for(let x = 0; x < countModules(); x++){
+    if(availableModules[x].name === "life-support"){
+      return x;
+    }
+  }
+}
+
+function loadModule(index){
+  let toLoadModule = availableModules[index];
+  toLoadModule.enabled = true;
+  ship.modules.push(toLoadModule);
+}
+
+loadModule(indexLifeSupport());
+
+function findModuleIndex(name){
+  for(let x = 0; x < countModules(); x++){
+    if(availableModules[x].name === name){
+      return x;
+    }
+  }
+}
+
+loadModule(findModuleIndex("propulsion"));
+
+loadModule(findModuleIndex("navigation"));
+
+function resetLARRY(){
+  for (let x = 0; x < 10; x++){
+    LARRY.quack();
+  }
+}
+
+resetLARRY();
+
+loadModule(findModuleIndex("communication"));
+
+function setMessage(){
+  radio.message = JSON.stringify(navigation);
+}
+
+setMessage();
+
+function activateBeacon(){
+  radio.beacon = true;
+}
+
+function setFrequency(){
+  radio.frequency = (radio.range.low + radio.range.high)/2;
+}
---
+++ day11/instructions.txt
@@ -0,0 +1,10 @@
+## Challenge #11
+"Beacon active!" LARRY sounds as pleased as a robot can be. In fact, you spot the faintest hint of smile on his robot duck bill.
+
+"Calibration QUACK complete! Now start RADIO! NEED to use the RADIO! QUACK QUACK!"
+
+LARRY's prompt sends you back to the ship's manual, where you find an entire section about the radio. You notice that the radio has a feature you missed before, the range! Your particular radio has a range of 88 to 108 MHz, much like the FM radio in your car back on Earth. There's a note in the manual that says that the radio should be tuned to the same frequency as the lower end plus the upper end, and that total divided by two.
+
+Write a function called setFrequency() that will set the frequency property on the radio object using the above formula.
+
+Be careful because not all radios will have the same range, so make sure your code is reuseable. Instead of coding the numbers directly into your equation use dot notation to access them from the radio object, which you can see by clicking the Show Global Objects link above.
